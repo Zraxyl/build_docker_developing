f0d889e4a47134bbafcb130146900f039e9bb5e8
Martinvlba <kailing2@live.com>
2023-04-09 10:48:43 +0000

    Update scriptlet to check peoper env

--- rootsys/locale.sh
+++ rootsys/locale.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-export TOOL_MAIN_NAME=EVOLIX
+export TOOL_MAIN_NAME=EVOLINX
+export TOOL_ENV_CHECK=none
+
+# Simple check for build env ( Check weather were in github workflow or in dev env )
+if [ -f /home/developer/EVOLINX/developing/rootsys/locale-gen ]; then
+    # Github env
+    export TOOL_ENV_CHECK=developing
+else
+    # Dev env
+    export TOOL_ENV_CHECK=build/docker/developing
+fi
 
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-cp -rf /home/developer/$TOOL_MAIN_NAME/tools/docker/locale-gen /usr/bin
+cp -rf /home/developer/$TOOL_MAIN_NAME/$TOOL_ENV_CHECK/rootsys/locale-gen /usr/bin
 chmod +x /usr/bin/locale-gen
 
-cp -rf /home/developer/$TOOL_MAIN_NAME/tools/docker/locale.gen /etc/locale.gen
+cp -rf /home/developer/$TOOL_MAIN_NAME/$TOOL_ENV_CHECK/rootsys/locale.gen /etc/locale.gen
 
 
 locale-gen
